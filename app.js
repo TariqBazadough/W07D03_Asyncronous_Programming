1b7041c5ac72fd31d2ae9b862761c8ab31a32043
TariqBazadough <tariq.bazadough@gmail.com>
2021-05-12 00:07:48 +0300

    done with pulse check question number 2

--- app.js
+++ app.js
@@ -6,6 +6,18 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
+const promiseFs = require("fs").promises
+
+const readFile = async () => {
+  try {
+    const data = await promiseFs.readFile("data.txt");
+    console.log(data.toString());
+  } catch (err) {
+    throw err;
+  }
+};
+
+readFile();
 
 app.listen(port, () => {
   console.log("W07D03_Asyncronous_Programming");
