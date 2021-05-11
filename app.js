1bb0c8a76b90e210cdb35aae9ddc1eac86830497
TariqBazadough <tariq.bazadough@gmail.com>
2021-05-11 23:53:02 +0300

    done with pulse check question number 1

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+const express = require("express");
+const fs = require("fs");
+const axios = require("axios");
 
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log("W07D03_Asyncronous_Programming");
+});
